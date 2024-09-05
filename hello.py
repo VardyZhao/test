10bf0ef9d1fb8a66575542792c0db5211f882eb1
Eden <abby@creatify.ai>
2024-09-05 23:59:00 +0800

    htest2

---
+++ hello.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    print('xxxx')
+
+
+    def test_hello():
+        print('hello')
+
+    def test_world():
+        print('world')
+
+    test_hello()
+    test_world()
